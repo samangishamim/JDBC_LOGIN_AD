fbb068af08ecc409363ba941e45b5718568e72c6
samangishamim <samangi624@gmail.com>
2024-01-12 15:30:25 +0330

    modify get address from user

--- JDBC_login_3-master/src/main/java/service/UserService.java
+++ JDBC_login_3-master/src/main/java/service/UserService.java
@@ -40,22 +40,20 @@ public class UserService {
         String username = scanner.nextLine();
 
         String password = "";
-        while(true) {
+        while (true) {
             System.out.println("Please enter your password:");
             password = scanner.nextLine();
-            if(Validation.isValidPassword(password)){
+            if (Validation.isValidPassword(password)) {
                 break;
-            }else{
+            } else {
                 System.out.println("Enter correct password");
             }
         }
 
+
         User user = new User(firstname, lastname, nationalId, username, password);
-        int result = userRepository.registerUser(user);
-        if (result == 1)
-            System.out.println(firstname + " you successfully register :)");
-        else
-            System.out.println("something is wrong :|");
+        int id = userRepository.registerUser(user);
+        getAddressFromUser(id);
     }
 
     public void signIn() throws SQLException {
@@ -73,27 +71,25 @@ public class UserService {
         else
             System.out.println("WELCOME " + user.getUsername());
     }
-    public  void getAddressFromUser () throws SQLException {
+
+    public void getAddressFromUser(int userid) throws SQLException {
         System.out.println("please enter your country: ");
-        String country =scanner.nextLine();
+        String country = scanner.nextLine();
         System.out.println("please enter your provinc: ");
-        String provinc =scanner.nextLine();
+        String provinc = scanner.nextLine();
         System.out.println("please enter your city: ");
-        String city =scanner.nextLine();
+        String city = scanner.nextLine();
         System.out.println("please enter your street: ");
-        String street =scanner.nextLine();
+        String street = scanner.nextLine();
         System.out.println("please enter your alley: ");
-        String alley =scanner.nextLine();
+        String alley = scanner.nextLine();
         System.out.println("please enter your number: ");
-        int number =scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("please enter your userid : ");
-        int userid =scanner.nextInt();
+        int number = scanner.nextInt();
         scanner.nextLine();
 
-        Address address=new Address(country,provinc,city,street,alley,number,userid);
-        int result =userRepository.addUserAddress(address);
-        if(result==1)
+        Address address = new Address(country, provinc, city, street, alley, number, userid);
+        int result = userRepository.addUserAddress(address);
+        if (result == 1)
             System.out.println("the address successfully added");
         else
             System.out.println("error");
