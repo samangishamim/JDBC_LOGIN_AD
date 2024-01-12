d9e8da0c33a6b39108c2679fa3f07359b749b9ab
samangishamim <samangi624@gmail.com>
2024-01-12 14:50:37 +0330

    add province field

--- JDBC_login_3-master/src/main/java/model/Address.java
+++ JDBC_login_3-master/src/main/java/model/Address.java
@@ -3,6 +3,7 @@ package model;
 public class Address {
     private int address_id;
     private String country;
+    private String province;
     private String city;
     private  String street;
     private String alley;
@@ -12,9 +13,10 @@ public class Address {
     public Address() {
     }
 
-    public Address(int address_id, String country, String city, String street, String alley, int number, int user_id) {
+    public Address(int address_id, String country, String province, String city, String street, String alley, int number, int user_id) {
         this.address_id = address_id;
         this.country = country;
+        this.province = province;
         this.city = city;
         this.street = street;
         this.alley = alley;
@@ -22,8 +24,9 @@ public class Address {
         this.user_id = user_id;
     }
 
-    public Address(String country, String city, String street, String alley, int number, int user_id) {
+    public Address(String country, String province, String city, String street, String alley, int number, int user_id) {
         this.country = country;
+        this.province = province;
         this.city = city;
         this.street = street;
         this.alley = alley;
@@ -45,6 +48,14 @@ public class Address {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
     }
 
     public String getCity() {
@@ -92,6 +103,7 @@ public class Address {
         return "\nAddress{" +
                 "address_id=" + address_id +
                 ", country='" + country + '\'' +
+                ", province='" + province + '\'' +
                 ", city='" + city + '\'' +
                 ", street='" + street + '\'' +
                 ", alley='" + alley + '\'' +
