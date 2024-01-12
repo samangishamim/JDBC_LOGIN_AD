8683aff9712e186f38bc6468ddc7f2c2d82f4c9a
samangishamim <samangi624@gmail.com>
2024-01-12 14:55:50 +0330

    fix  and add address table

--- JDBC_login_3-master/src/main/java/sql/table.sql
+++ JDBC_login_3-master/src/main/java/sql/table.sql
@@ -1,8 +1,20 @@
-CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id     SERIAL PRIMARY KEY,
+    first_name  VARCHAR(255),
+    last_name   VARCHAR(255),
     national_id VARCHAR(255),
-    username VARCHAR(255),
-    password VARCHAR(255)
-);
+    username    VARCHAR(255),
+    password    VARCHAR(255)
+    );
+CREATE TABLE IF NOT EXISTS address
+(
+    address_id SERIAL PRIMARY KEY,
+    country    VARCHAR(50),
+    province   VARCHAR(50),
+    city       VARCHAR(50),
+    street     VARCHAR(50),
+    alley      VARCHAR(50),
+    number     int,
+    user_id int references users(user_id)
+    );
