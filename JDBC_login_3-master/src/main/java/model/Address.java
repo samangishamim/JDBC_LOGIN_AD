e91284c9b7790b1f6c4a37b91889802c32c86a66
samangishamim <samangi624@gmail.com>
2024-01-12 14:37:03 +0330

    add needed fields

--- JDBC_login_3-master/src/main/java/model/Address.java
+++ JDBC_login_3-master/src/main/java/model/Address.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Address {
+    private int address_id;
+    private String country;
+    private String city;
+    private  String street;
+    private String alley;
+    private  int number;
+    private int user_id;
+
+
+
 }
