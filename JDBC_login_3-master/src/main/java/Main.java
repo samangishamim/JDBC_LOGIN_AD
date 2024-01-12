4585545feabea8c9ec11424643f704bad207d271
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-12 15:43:51 +0330

    add hello

--- JDBC_login_3-master/src/main/java/Main.java
+++ JDBC_login_3-master/src/main/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         Menu menu = new Menu();
         menu.publicMenu();
+        System.out.println("hello");
     }
 }
