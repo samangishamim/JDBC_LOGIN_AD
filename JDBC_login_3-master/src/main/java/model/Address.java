ec7774391fe7a83438d4b039227fb2bed2e8271a
samangishamim <samangi624@gmail.com>
2024-01-12 14:38:44 +0330

    add no arg and all arg constructor and all arg without address_id

--- JDBC_login_3-master/src/main/java/model/Address.java
+++ JDBC_login_3-master/src/main/java/model/Address.java
@@ -9,6 +9,25 @@ public class Address {
     private  int number;
     private int user_id;
 
+    public Address() {
+    }
 
+    public Address(int address_id, String country, String city, String street, String alley, int number, int user_id) {
+        this.address_id = address_id;
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.alley = alley;
+        this.number = number;
+        this.user_id = user_id;
+    }
 
+    public Address(String country, String city, String street, String alley, int number, int user_id) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.alley = alley;
+        this.number = number;
+        this.user_id = user_id;
+    }
 }
